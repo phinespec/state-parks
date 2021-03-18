05e42db06743b0d57baf64f83da1314bc24495b8
Phil Trunnell <phinespecimen@gmail.com>
2021-03-18 11:50:30 -0600

    Add recycler with grid layout for activities fragment and have it pull from DB

--- app/src/main/java/com/example/stateparks/adapters/ActivitiesRecyclerAdapter.kt
+++ app/src/main/java/com/example/stateparks/adapters/ActivitiesRecyclerAdapter.kt
@@ -1,18 +1,32 @@
 package com.example.stateparks.adapters
 
 import android.content.Context
+import android.media.Image
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.stateparks.R
 import com.example.stateparks.data.Activity
+import kotlinx.android.synthetic.main.activity_item.view.*
 
 class ActivitiesRecyclerAdapter(val context: Context, val activitiesList: List<Activity>):
     RecyclerView.Adapter<ActivitiesRecyclerAdapter.ActivitiesViewHolder>() {
 
     inner class ActivitiesViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
-        val 
+        val activityTitle: TextView = itemView.itemText
+        val activityImage: ImageView = itemView.itemImage
+
+        init {
+            itemView.setOnClickListener { v: View ->
+                val position: Int = adapterPosition
+                val activity = activitiesList[position]
+                Log.i("info", "Activity ${position}")
+            }
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ActivitiesViewHolder {
@@ -24,6 +38,15 @@ class ActivitiesRecyclerAdapter(val context: Context, val activitiesList: List<A
 
     override fun onBindViewHolder(holder: ActivitiesViewHolder, position: Int) {
         val activity = activitiesList.get(position)
+
+        val imageList = listOf(
+            R.drawable.offroading, R.drawable.boating, R.drawable.camping,
+            R.drawable.golf, R.drawable.fishing, R.drawable.hiking,
+            R.drawable.mountain_biking, R.drawable.museums, R.drawable.dark_sky, R.drawable.jr_ranger
+        )
+
+        holder.activityTitle.text = activity?.title
+        holder.activityImage.setImageResource(imageList[position])
     }
 
     override fun getItemCount(): Int {
--- app/src/main/java/com/example/stateparks/view/activities/ActivitiesFragment.kt
+++ app/src/main/java/com/example/stateparks/view/activities/ActivitiesFragment.kt
@@ -6,9 +6,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.stateparks.R
+import com.example.stateparks.adapters.ActivitiesRecyclerAdapter
+import com.example.stateparks.adapters.ParksRecyclerAdapter
 import com.example.stateparks.data.ParksDatabase
 
 class ActivitiesFragment : Fragment() {
@@ -30,6 +33,8 @@ class ActivitiesFragment : Fragment() {
                 this, viewModelFactory
             ).get(ActivitiesViewModel::class.java)
 
+
+
         val activities = activitiesViewModel.database.getAllActivities()
         val recyclerView = binding.activitiesRecycler
 
@@ -39,6 +44,10 @@ class ActivitiesFragment : Fragment() {
         val gridLayoutManager = GridLayoutManager(context, 3)
         recyclerView.layoutManager = gridLayoutManager
         recyclerView.setHasFixedSize(true)
+
+        activities.observe(viewLifecycleOwner, Observer { activitiesList ->
+            recyclerView.adapter = ActivitiesRecyclerAdapter(requireContext(), activitiesList)
+        })
 
 
         return binding.root
