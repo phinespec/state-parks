392e3cfeab004e3ff2417f87f1e328be0f447fc8
Phil Trunnell <phinespecimen@gmail.com>
2021-03-27 11:39:10 -0600

    Update UI with better layout and theme

--- app/src/main/java/com/example/stateparks/view/home/HomeFragment.kt
+++ app/src/main/java/com/example/stateparks/view/home/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.example.stateparks.view.home
 
+
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,9 +11,9 @@ import com.example.stateparks.R
 import com.example.stateparks.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
-    //    private lateinit var parksViewModel: ParksViewModel
 
     private lateinit var binding: FragmentHomeBinding
+    private lateinit var viewModel: HomeViewModel
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
@@ -20,5 +21,11 @@ class HomeFragment : Fragment() {
             inflater, R.layout.fragment_home, container, false)
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val viewModel = binding.viewModel
+        viewModel?.fadeInText(binding.subHeading)
     }
 }
--- app/src/main/java/com/example/stateparks/view/home/HomeViewModel.kt
+++ app/src/main/java/com/example/stateparks/view/home/HomeViewModel.kt
@@ -1,4 +1,13 @@
 package com.example.stateparks.view.home
 
+import android.animation.ObjectAnimator
+import android.view.View
+
 class HomeViewModel {
+
+    fun fadeInText(view: View) {
+        val animator = ObjectAnimator.ofFloat(view, View.ALPHA, 0f).apply {
+            start()
+        }
+    }
 }
